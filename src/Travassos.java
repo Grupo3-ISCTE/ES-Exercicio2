2a912f9cab5ec1280e09daab3cf7b9983c762cfe
jecsct <jetravassos2001@gmail.com>
2022-03-07 20:48:28 +0000

    Primeiro Commit Joao Travassos

--- src/Travassos.java
+++ src/Travassos.java
@@ -1,6 +1,5 @@
-
 public class Travassos {
-	public static void main(String[] args) {
-		System.out.println("Joao Travassos - 93089");
-	}
+    public static void main(String[] args) {
+        System.out.println("Joao Travassos - 93089");
+    }
 }
