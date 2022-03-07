babb117df3a067eece5015baffd85ded3166d7f6
apsoo-iscte <apsoo@iscte-iul.pt>
2022-03-07 18:05:54 +0000

    1o commit na 2a branch do afonso

--- src/Afonso.java
+++ src/Afonso.java
@@ -1,5 +1,7 @@
 public class Afonso {
     public static void main(String[] args) {
         System.out.println("Hello World! - Afonso");
+        System.out.println("Primeiro commit no 2 brunch");
     }
+
 }
